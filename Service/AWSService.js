585519d02c560b3fc3ba942e3d049969d7f07ba7
Saymon Oliveira de Castro <saymon_castro@estudante.sesisenai.org.br>
2025-03-04 18:10:30 -0300

    Update AWSService.js

--- Service/AWSService.js
+++ Service/AWSService.js
@@ -1,18 +1,22 @@
 const AWSRepository = require('../Repository/AWSRepository');
 const path = require('path');
 const fs = require('fs');
+const crypto = require('crypto');
 
 class AWSService {
     async buscarImagem(referencia) {
         return await AWSRepository.buscarImagem(referencia);
     }
 
-    async uploadImagem(file) {
-        return await AWSRepository.uploadImagem(file);
+    async uploadImagem(file, id) {
+        const referencia = crypto.randomUUID();
+        const url = await AWSRepository.uploadImagem(file, id, referencia);
+        return url;
     }
 
     async downloadImagem(referencia) {
         const fileData = await AWSRepository.downloadImagem(referencia);
+
         const downloadsPath = path.join(require('os').homedir(), 'Downloads');
         const filePath = path.join(downloadsPath, referencia);
 
